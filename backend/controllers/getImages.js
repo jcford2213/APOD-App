3c13e433c2d0b2eac896844e5a5e8b98502068e4
IdioticMarlin22 <jcrantfo@comcast.net>
2022-12-30 15:57:00 -0500

    Put backend back to normal with axios

--- backend/controllers/getImages.js
+++ backend/controllers/getImages.js
@@ -1,23 +1,22 @@
 // DEPENDENCIES & IMPORTS
-//import axios from 'axios';
 import * as dotenv from 'dotenv';
+import axios from 'axios';
 dotenv.config();
 
 // Gets today's APOD data from NASA's api
 // Returns an object
 export const getTodaysImage = async () => {
-  return('This worked')
-  // const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`;
-  // const apiResult = await axios(url)
-  //   .then(res => {
-  //     console.log(`Sending today's image`);
-  //     return res.data
-  //   })  //returns hdurl which is the key for the image link
-  //   .catch(error => {
-  //     console.log(`Error with Axios Call: ${error.message}`)
-  //     return 'Axios Error'
-  //   });
-  //   return apiResult
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`;
+  const apiResult = await axios(url)
+    .then(res => {
+      console.log(`Sending today's image`);
+      return res.data
+    })  //returns hdurl which is the key for the image link
+    .catch(error => {
+      console.log(`Error with Axios Call: ${error.message}`)
+      return 'Axios Error'
+    });
+    return apiResult
 }
 
 // Serves a different image for a user selected date
