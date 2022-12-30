5cf8685e18d7b74c347baa85598bf904c83ec47f
IdioticMarlin22 <jcrantfo@comcast.net>
2022-12-30 15:40:29 -0500

    Removed getImageSize.js Testing getImage without Axios in the way

--- backend/controllers/getImages.js
+++ backend/controllers/getImages.js
@@ -1,22 +1,23 @@
 // DEPENDENCIES & IMPORTS
-import axios from 'axios';
+//import axios from 'axios';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 // Gets today's APOD data from NASA's api
 // Returns an object
 export const getTodaysImage = async () => {
-  const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`;
-  const apiResult = await axios(url)
-    .then(res => {
-      console.log(`Sending today's image`);
-      return res.data
-    })  //returns hdurl which is the key for the image link
-    .catch(error => {
-      console.log(`Error with Axios Call: ${error.message}`)
-      return 'Axios Error'
-    });
-    return apiResult
+  return('This worked')
+  // const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`;
+  // const apiResult = await axios(url)
+  //   .then(res => {
+  //     console.log(`Sending today's image`);
+  //     return res.data
+  //   })  //returns hdurl which is the key for the image link
+  //   .catch(error => {
+  //     console.log(`Error with Axios Call: ${error.message}`)
+  //     return 'Axios Error'
+  //   });
+  //   return apiResult
 }
 
 // Serves a different image for a user selected date
